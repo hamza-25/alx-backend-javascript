d1145c61b3556f32ff90f68bea9731dcc32dad03
hamza-25 <me677499@gmail.com>
2024-03-04 15:23:09 +0100

    for of es6

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const array2 = [];
+  for (const element of array) {
+    array2.push(`${appendString}${element}`);
+  }
+  return array2;
+}
