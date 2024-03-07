08a8aa7d58e45ba2adc3f4d83800cdc47c170876
hamza-25 <me677499@gmail.com>
2024-03-07 20:35:26 +0100

    update 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -21,11 +21,32 @@ export default class HolbertonCourse {
     return this._name;
   }
 
+  set name(name) {
+    if (typeof (name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
   get length() {
     return this._length;
   }
 
+  set length(length) {
+    if (typeof (length) !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
   get students() {
     return this._students;
+  }
+
+  set students(students) {
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    }
+    this._students = students;
   }
 }
