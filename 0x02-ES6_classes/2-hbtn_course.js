7edf1f2f357d8bec03008db91e220eb9eda853f7
hamza-25 <me677499@gmail.com>
2024-03-07 20:31:38 +0100

    update 2

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== 'string') {
-      throw Error('Name must be a string');
+      throw new TypeError('Name must be a string');
     } else {
       this._name = name;
     }
     if (typeof (length) !== 'number') {
-      throw Error('Length must be a number');
+      throw new TypeError('Length must be a number');
     } else {
       this._length = length;
     }
