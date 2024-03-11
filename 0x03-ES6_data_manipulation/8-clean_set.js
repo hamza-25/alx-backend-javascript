687874903cc091412d92904db19cb536d8681ccf
hamza-25 <me677499@gmail.com>
2024-03-11 16:49:35 +0000

    fix some bugs in 8 task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(setArray, startString) {
-  if (startString === '') return '';
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
   const array = Array.from(setArray);
   const startStringLenght = startString.length;
   const newArray = [];
   array.forEach((element) => {
-    if (element.slice(0, startStringLenght) === startString) {
-      const add = element.slice(startStringLenght, element.length);
+    if (element.startsWith(startString)) {
+      const add = element.slice(startStringLenght);
       newArray.push(add);
     }
   });
