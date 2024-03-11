b06ae5888bf0b69e1c3c4137f6dd12602932497b
hamza-25 <me677499@gmail.com>
2024-03-11 17:41:10 +0000

    add throw for error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,11 @@
 export default function updateUniqueItems(map) {
-  map.forEach((element, index) => {
-    if (Number(element) === 1) {
-      map.set(index, 100);
-    }
-  });
-  return map;
+  if (map instanceof Map) {
+    map.forEach((element, index) => {
+      if (Number(element) === 1) {
+        map.set(index, 100);
+      }
+    });
+    return map;
+  }
+  throw new Error('Cannot process');
 }
