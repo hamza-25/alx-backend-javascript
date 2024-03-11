36fda815bb6e2e3e4271eb7fae1f19ec74cab01b
hamza-25 <me677499@gmail.com>
2024-03-11 17:49:29 +0000

    update task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,8 @@
 export default function cleanSet(setArray, startString) {
-  if (startString === undefined || startString.length === 0) {
-    return '';
-  }
-  const array = Array.from(setArray);
+  if (startString === undefined || startString.length === 0) return '';
   const startStringLenght = startString.length;
   const newArray = [];
-  array.forEach((element) => {
+  setArray.forEach((element) => {
     if (element.startsWith(startString)) {
       const add = element.slice(startStringLenght);
       newArray.push(add);
