7ec97ff5a66a9c8353beb2d11230ac99724bc89f
hamza-25 <me677499@gmail.com>
2024-03-11 18:13:20 +0000

    update task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,19 @@
-// export default function cleanSet(setArray, startString) {
-//   if (startString === undefined || startString.length === 0) return '';
-//   const startStringLenght = startString.length;
-//   const newArray = [];
-//   setArray.forEach((element) => {
-//     if (element.startsWith(startString)) {
-//       const add = element.slice(startStringLenght);
-//       newArray.push(add);
-//     }
-//   });
-//   return newArray.join('-');
-// }
-export default function cleanSet(set, startString) {
+export default function cleanSet(setArray, startString) {
   if (startString === undefined || startString.length === 0) return '';
-  return [...set]
-    .filter((element) => (element !== undefined ? element.startsWith(startString) : ''))
-    .map((element) => (element !== undefined ? element.slice(startString.length) : ''))
-    .join('-');
+  const startStringLenght = startString.length;
+  const newArray = [];
+  setArray.forEach((element) => {
+    if (element.startsWith(startString) && element !== undefined) {
+      const add = element.slice(startStringLenght);
+      newArray.push(add);
+    }
+  });
+  return newArray.join('-');
 }
+// export default function cleanSet(set, startString) {
+//   if (startString === undefined || startString.length === 0) return '';
+//   return [...set]
+//     .filter((element) => (element !== undefined ? element.startsWith(startString) : ''))
+//     .map((element) => (element !== undefined ? element.slice(startString.length) : ''))
+//     .join('-');
+// }
