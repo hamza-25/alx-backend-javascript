ff7548c5602e77c9a4872ef0867326ba1939f71b
hamza-25 <me677499@gmail.com>
2024-04-29 23:09:30 +0100

    up

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,37 +1,3 @@
-// const fs = require('fs')
-// const { parse } = require('csv-parse')
-
-
-// function countStudents(file_path) {
-//     const field = {};
-//     let total = 0;
-
-//     try {
-//         fs.createReadStream(file_path)
-//             .pipe(parse({ delimiter: ',', from_line: 2 }))
-//             .on('data', function (row) {
-//                 if (row[3] in field) {
-//                     field[row[3]]['count'] += 1;
-//                     total += 1;
-//                     field[row[3]]['names'].push(row[0]);
-//                 } else {
-//                     field[row[3]] = { 'count': 1, 'names': [row[0]] };
-//                     total += 1;
-//                 }
-//             }).on('error', function (error) {
-//                 throw Error('Cannot load the database');
-//             }).on("end", function () {
-//                 process.stdout.write(`Number of students: ${total}\n`);
-//                 Object.entries(field).forEach(([key, value]) => {
-//                     process.stdout.write(`Number of students in ${key}: ${field[key]['count']}. List: ${field[key]['names'].join(', ')}\n`);
-//                 })
-//             });
-//     } catch (err) {
-//         throw Error('Cannot load the database');
-
-//     }
-// }
-// module.exports = countStudents;
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
@@ -53,8 +19,8 @@ function countStudents(file_path) {
                 total += 1;
             }
         })
-        .on('error', function (error) {
-            throw Error('Cannot load the database');
+        .on('error', (error) => {
+            throw Error("Custom error message");
         })
         .on('end', function () {
             console.log(`Number of students: ${total}`);
