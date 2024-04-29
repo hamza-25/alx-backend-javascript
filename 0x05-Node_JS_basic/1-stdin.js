7aa6d102166979032d5db43579590055e38e6cd5
hamza-25 <me677499@gmail.com>
2024-04-29 19:37:50 +0100

    fix bugs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,19 @@
-const readline = require('node:readline')
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+process.stdout.write(`Welcome to Holberton School, what is your name?\n`)
 
-for (let i = 0; i < process.argv.length; i++) {
-    rl.question(`Welcome to Holberton School, what is your name?\n`, name => {
-        console.log(`Your name is: ${name}`);
-        rl.close();
-    });
-
-}
-
-if (!process.stdin.isTTY) {
-    process.on('exit', function (code) {
-        console.log('This important software is now closing');
+if(process.stdin.isTTY){
+    process.stdin.on('data', (data) => {
+        process.stdout.write(`Your name is: ${data}`);
+        process.exit();
     });
 }
+else {
+    process.stdin.on('data', (data) => {
+        process.stdout.write(`Your name is: ${data}`);
+        process.exit();
+    });
+    process.on('exit', () => {
+        process.stdout.write(`This important software is now closing\n`);
+    });
+}
+
 
