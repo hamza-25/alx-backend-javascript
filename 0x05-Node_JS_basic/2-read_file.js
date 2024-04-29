fc561e6cdf197ececbed818ceaff54279bec6985
hamza-25 <me677499@gmail.com>
2024-04-29 22:35:03 +0100

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,67 @@
-const fs = require('fs')
-const { parse } = require('csv-parse')
+// const fs = require('fs')
+// const { parse } = require('csv-parse')
 
-const field = {};
-let total = 0;
 
-fs.createReadStream('database.csv')
-    .pipe(parse({ delimiter: ',', from_line: 2 }))
-    .on('data', function (row) {
-        if (row[3] in field) {
-            field[row[3]]['count'] += 1
-            total += 1;
-            field[row[3]]['names'].push(row[0])
-        } else {
-            field[row[3]] = { 'count': 1, 'names': [row[0]] }
-            total += 1;
-        }
-    }).on('error', function (error) {
-        console.log('Cannot load the database');
-    }).on("end", function () {
-        process.stdout.write(`Number of students: ${total}\n`)
-        Object.entries(field).forEach(([key, value]) => {
-            process.stdout.write(`Number of students in ${key}: ${field[key]['count']}. List: ${field[key]['names'].join(', ')}\n`)
+// function countStudents(file_path) {
+//     const field = {};
+//     let total = 0;
+
+//     try {
+//         fs.createReadStream(file_path)
+//             .pipe(parse({ delimiter: ',', from_line: 2 }))
+//             .on('data', function (row) {
+//                 if (row[3] in field) {
+//                     field[row[3]]['count'] += 1;
+//                     total += 1;
+//                     field[row[3]]['names'].push(row[0]);
+//                 } else {
+//                     field[row[3]] = { 'count': 1, 'names': [row[0]] };
+//                     total += 1;
+//                 }
+//             }).on('error', function (error) {
+//                 throw Error('Cannot load the database');
+//             }).on("end", function () {
+//                 process.stdout.write(`Number of students: ${total}\n`);
+//                 Object.entries(field).forEach(([key, value]) => {
+//                     process.stdout.write(`Number of students in ${key}: ${field[key]['count']}. List: ${field[key]['names'].join(', ')}\n`);
+//                 })
+//             });
+//     } catch (err) {
+//         throw Error('Cannot load the database');
+
+//     }
+// }
+// module.exports = countStudents;
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+function countStudents(file_path) {
+    const field = {};
+    let total = 0;
+
+    const stream = fs.createReadStream(file_path);
+
+    stream
+        .pipe(parse({ delimiter: ',', from_line: 2 }))
+        .on('data', function (row) {
+            if (row[3] in field) {
+                field[row[3]]['count'] += 1;
+                total += 1;
+                field[row[3]]['names'].push(row[0]);
+            } else {
+                field[row[3]] = { 'count': 1, 'names': [row[0]] };
+                total += 1;
+            }
         })
-    });
+        .on('error', function (error) {
+            throw Error('Cannot load the database');
+        })
+        .on('end', function () {
+            console.log(`Number of students: ${total}`);
+            Object.entries(field).forEach(([key, value]) => {
+                console.log(`Number of students in ${key}: ${field[key]['count']}. List: ${field[key]['names'].join(', ')}`);
+            });
+        });
+}
+
+module.exports = countStudents;
