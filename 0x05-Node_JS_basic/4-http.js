ac3061e6913ed41ebdcefcc6a0f484126078b15d
hamza-25 <me677499@gmail.com>
2024-04-30 19:40:41 +0100

    update variable

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
 });
 
 const PORT = 1245;
-server.listen(PORT, () => {
-    console.log('...');
-});
+const HOST = '127.0.0.1';
+
+app.listen(PORT, HOST,() => {});
+
+module.exports = app;
