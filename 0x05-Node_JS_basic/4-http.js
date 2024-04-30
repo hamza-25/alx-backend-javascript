64a559ac1215dc4dd2224f988740eb940d9194c5
hamza-25 <me677499@gmail.com>
2024-04-30 21:18:17 +0100

    eslint fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 const PORT = 1245;
 const HOST = '127.0.0.1';
 
-app.listen(PORT, HOST,() => {});
+app.listen(PORT, HOST, () => {});
 
 module.exports = app;
--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,59 +1,58 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 const port = 1245;
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
-
-async function countStudents(file_path) {
+async function countStudents(filePath) {
   const field = {};
   let total = 0;
 
-  const stream = fs.createReadStream(file_path);
+  const stream = fs.createReadStream(filePath);
 
   return await new Promise((resolve, reject) => {
-      stream
-          .pipe(parse({ delimiter: ',', from_line: 2 }))
-          .on('data', function (row) {
-              if (row[3] in field) {
-                  field[row[3]]['count'] += 1;
-                  total += 1;
-                  field[row[3]]['names'].push(row[0]);
-              } else {
-                  field[row[3]] = { 'count': 1, 'names': [row[0]] };
-                  total += 1;
-              }
-          })
-          .on('error', (error) => {
-              reject(new Error("Custom error message"));
-          })
-          .on('end', function () {
-              let str = `Number of students: ${total}\n`;
-              Object.entries(field).forEach(([key, value]) => {
-                  str += `Number of students in ${key}: ${field[key]['count']}. List: ${field[key]['names'].join(', ')}\n`;
-              });
-              resolve(str);
-          });
+    stream
+      .pipe(parse({ delimiter: ',', from_line: 2 }))
+      .on('data', (row) => {
+        if (row[3] in field) {
+          field[row[3]].count += 1;
+          total += 1;
+          field[row[3]].names.push(row[0]);
+        } else {
+          field[row[3]] = { count: 1, names: [row[0]] };
+          total += 1;
+        }
+      })
+      .on('error', () => { // error
+        reject(new Error('Custom error message'));
+      })
+      .on('end', () => {
+        let str = `Number of students: ${total}\n`;
+        Object.entries(field).forEach(([key]) => { // value
+          str += `Number of students in ${key}: ${field[key].count}. List: ${field[key].names.join(', ')}\n`;
+        });
+        resolve(str);
+      });
   });
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.get('/students', (req, res) => {
   countStudents(process.argv[2].toString())
-            .then((output) => {
-                const outString = output.slice(0, -1);
-                res.send(outString)
-            })
-            .catch(() => {
-                res.statusCode = 404;
-                res.end('Cannot load the database');
-            });
-})
+    .then((output) => {
+      const outString = output.slice(0, -1);
+      res.send(outString);
+    })
+    .catch(() => {
+      res.statusCode = 404;
+      res.end('Cannot load the database');
+    });
+});
 
-app.listen(port, () => {})
+app.listen(port, () => {});
 
 module.exports = app;
