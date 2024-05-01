2c010eb0b406aa819fca739ade37adf4a61b561b
hamza-25 <me677499@gmail.com>
2024-05-01 15:45:22 +0100

    add test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,9 +1,16 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
-const { it, describe} = require('mocha');
+// const { it, describe} = require('mocha');
 
 describe('function that add two number', () => {
     it('should return addition numbers', () => {
-        assert.equal(calculateNumber(4, 5), 9);
+        assert.strictEqual(calculateNumber(4, 5), 9);
     });
+    it('around numbers', () => {
+        assert.strictEqual(calculateNumber(1.6, 1.2), 3);
+    });
+    it('around numbers negative', () => {
+        assert.strictEqual(calculateNumber(-1.6, -1.2), -3);
+    });
+
 });
