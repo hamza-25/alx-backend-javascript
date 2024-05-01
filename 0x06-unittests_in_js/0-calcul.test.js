dad717ba9d8e1be710f69ab7bd71d8343c2e78be
hamza-25 <me677499@gmail.com>
2024-05-01 15:30:54 +0100

    fix some bugs

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
-const sum = require('./0-calcul');
+const calculateNumber = require('./0-calcul');
+const { it, describe} = require('mocha');
 
 describe('function that add two number', () => {
     it('should return addition numbers', () => {
-        assert.equal(sum(4, 5), 9);
+        assert.equal(calculateNumber(4, 5), 9);
     });
 });
