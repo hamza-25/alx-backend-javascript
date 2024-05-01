acf6538095f2680b59632f36c5312d414c6fa4e7
hamza-25 <me677499@gmail.com>
2024-05-01 20:12:37 +0100

    add stdout in func

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,6 @@ const Utils = require('./utils');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping){
     const sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    return sum;
     console.log(`The total is: ${sum}`);
 }
 
