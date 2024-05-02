c37e418eb28ba1381bc205bad380ab74e8ca447a
hamza-25 <me677499@gmail.com>
2024-05-02 17:16:26 +0100

    up2

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 describe('test fetch api', () => {
     it('fetching api with promise', () => {
         getPaymentTokenFromAPI(true).then((data) => {
-            assert.equal(data, {data: 'Successful response from the API' });
+            assert.equal(data.data, 'Successful response from the API');
             done();
         })
     });
