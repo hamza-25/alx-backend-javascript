ca01bd035e05e9f81dfe71366d445574652acd0f
hamza-25 <me677499@gmail.com>
2024-05-02 17:39:43 +0100

    fix eslint

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,11 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
-const {describe, it} = require("mocha");
 const assert = require('assert');
-const expect = require("chai").expect;
+const { it, describe } = require('mocha');
 
 describe('test fetch api', () => {
-    it('fetching api with promise', () => {
+    it('fetching api with promise', (done) => {
         getPaymentTokenFromAPI(true).then((data) => {
-            // assert.equal(data.data, 'Successful response from the API');
-            expect(data).to.have.property('data');
+            assert.equal(data, {data: 'Successful response from the API'});
             done();
         });
     });
