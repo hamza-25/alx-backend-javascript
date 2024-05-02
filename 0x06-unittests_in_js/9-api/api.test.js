25726e883c9f31103c62efa767556db4683203dd
hamza-25 <me677499@gmail.com>
2024-05-02 19:49:44 +0100

    up1

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,6 +1,5 @@
 const request = require('request');
 const {it, describe} = require("mocha");
-// const assert = require('assert');
 const expect = require("chai").expect;
 
 describe('index page', () => {
@@ -10,21 +9,18 @@ describe('index page', () => {
     };
     it('testing status code 200', (done) => {
         request(option, (err, res) => {
-            // assert.equal(res.statusCode, 200);
             expect(res.statusCode).to.equal(200);
         });
         done();
     });
     it('testing status code 404', (done) => {
         request(option, (err, res, body) => {
-            //   assert.equal(body, 'Welcome to the payment system');
             expect(body).to.contain("Welcome to the payment system");
         });
         done();
     });
     it('testing body lenght', (done) => {
         request(option, (err, res) => {
-            //   assert.equal(res.headers['content-length'], 29);
               expect(res.headers['content-length']).to.equal('29');
         });
         done();
@@ -40,19 +36,14 @@ describe('cart testing', () => {
     });
     it('testing status code 404', (done) => {
         request('http://localhost:7865/cart/12', (err, res, body) => {
-            //   assert.equal(body, 'Welcome to the payment system');
             expect(body).to.contain(`Payment methods for cart 12`);
         });
         done();
     });
     it('testing body lenght', (done) => {
         request('http://localhost:7865/cart/aString', (err, res) => {
-            //   assert.equal(res.headers['content-length'], 29);
-            //   expect(res.headers['content-length']).to.equal('29');
             expect(res.statusCode).to.equal(404);
         });
         done();
     });
-
-    
 });
