c8caac1029cfa2c7efb76f263e65893c50d527cf
hamza-25 <me677499@gmail.com>
2024-05-02 18:45:39 +0100

    f up

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const {it, describe} = require("mocha");
-const assert = require('assert');
+// const assert = require('assert');
+const expect = require("chai").expect;
 
 describe('index page', () => {
     const option = {
@@ -8,20 +9,23 @@ describe('index page', () => {
         method: 'GET',
     };
     it('testing status code', (done) => {
-        request(option, (res) => {
-            assert.equal(res.statusCode, 200);
+        request(option, (err, res, body) => {
+            // assert.equal(res.statusCode, 200);
+            expect(res.statusCode).to.equal(200);
         });
         done();
     });
     it('testing body', (done) => {
-        request(option, (body) => {
-              assert.equal(body, 'Welcome to the payment system');
+        request(option, (err, res, body) => {
+            //   assert.equal(body, 'Welcome to the payment system');
+            expect(body).to.contain("Welcome to the payment system");
         });
         done();
     });
     it('testing body lenght', (done) => {
-        request(option, (res) => {
-              assert.equal(res.headers['content-length'], 29);
+        request(option, (err, res, body) => {
+            //   assert.equal(res.headers['content-length'], 29);
+              expect(res.headers['content-length']).to.equal('29');
         });
         done();
     });
