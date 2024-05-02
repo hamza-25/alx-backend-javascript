18436fc054b94ab4d6d26a1a68e0d58e51627a7b
hamza-25 <me677499@gmail.com>
2024-05-02 17:57:16 +0100

    update1

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,8 +2,8 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 const assert = require('assert');
 const { it, describe } = require('mocha');
 
-describe('test fetch api', function() {
-    it('fetching api with promise', function() {
+describe('test fetch api', () => {
+    it('fetching api with promise', (done) => {
         getPaymentTokenFromAPI(true).then((data) => {
             assert.equal(data, {data: 'Successful response from the API'});
             done();
