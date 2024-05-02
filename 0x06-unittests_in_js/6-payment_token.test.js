a21e27c0018937efb7ff864f4a3ca89d83243ac1
hamza-25 <me677499@gmail.com>
2024-05-02 17:58:22 +0100

    up2

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,6 +1,6 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
 const assert = require('assert');
-const { it, describe } = require('mocha');
+// const { it, describe } = require('mocha');
 
 describe('test fetch api', () => {
     it('fetching api with promise', (done) => {
