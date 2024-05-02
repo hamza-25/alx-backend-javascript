de62b889f27b9545af3ab76ba29d5e35a1eadaf0
hamza-25 <me677499@gmail.com>
2024-05-02 17:48:40 +0100

    update

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,6 +1,6 @@
 const request = require('request');
-const chai = require('chai');
-const {describe, it} = require("mocha");
+// const chai = require('chai');
+// const {describe, it} = require("mocha");
 const assert = require('assert');
 
 describe('index page', () => {
@@ -9,19 +9,19 @@ describe('index page', () => {
         method: 'GET',
     };
     it('testing status code', (done) => {
-        const req = request(option, (err, res, body) => {
+        request(option, (res) => {
             assert.equal(res.statusCode, 200);
         });
         done();
     });
     it('testing body', (done) => {
-        const req = request(option, (err, res, body) => {
+        request(option, (body) => {
               assert.equal(body, 'Welcome to the payment system');
         });
         done();
     });
     it('testing body lenght', (done) => {
-        const req = request(option, (err, res, body) => {
+        request(option, (res) => {
               assert.equal(res.headers['content-length'], 29);
         });
         done();
